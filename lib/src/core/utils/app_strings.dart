7a68cbf6eb8958b6f6d1aad88da3bf15fee2ca02
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-21 00:23:03 +0300

    Installed new package & Updated Assets, AppColors, AppStrings

--- lib/src/core/utils/app_strings.dart
+++ lib/src/core/utils/app_strings.dart
@@ -3,4 +3,10 @@ class AppStrings {
 
   static const String appTitle = 'Carey';
   static const String fontFamily = 'Inter';
+  static const String welcomeTo = 'Welcome to';
+  static const String welcomeViewDescription =
+      "The best car marketplace app of the \n century for your transportation needs!";
+  static const String startViewDescription =
+      "The best car in your \n hands with Carey";
+  static const String getStarted = 'Get Started';
 }
