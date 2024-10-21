5d8156fa6085921b3ba6f7828442d27c9de8668d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-21 13:31:41 +0300

    Added SharedPrefHelper & Updated DI  - Installed shared_preferences package  - Updated main

--- lib/src/core/di/dependency_injection.dart
+++ lib/src/core/di/dependency_injection.dart
@@ -1,12 +1,20 @@
 import 'package:carey/src/core/router/app_router.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final GetIt getIt = GetIt.instance;
 
-void setupDI() {
+Future<void> setupDI() async {
+  await _setupForExternal();
   _setupDIForCore();
 }
 
 void _setupDIForCore() {
   getIt.registerSingleton<AppRouter>(AppRouter());
+}
+
+Future<void> _setupForExternal() async {
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
 }
