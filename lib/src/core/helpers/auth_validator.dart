b66817086932003cec6e344927a1d871e61d25b6
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-21 15:01:21 +0300

    Added AuthValidatorRegex, AuthValidator & Added global_options to build.yaml

---
+++ lib/src/core/helpers/auth_validator.dart
@@ -0,0 +1,31 @@
+import 'package:carey/src/core/helpers/auth_validator_regex.dart';
+import 'package:carey/src/core/utils/app_strings.dart';
+import 'package:flutter/material.dart';
+
+class AuthValidator {
+  static String? validatePasswordField(BuildContext context, {String? value}) {
+    if (value == null || value.isEmpty) {
+      return AppStrings.passwordRequired;
+    } else if (value.length < 8) {
+      return AppStrings.passwordInvalidLength;
+    } else if (!AuthValidatorRegex.passwordHasCapitalCharacter(value)) {
+      return AppStrings.passwordContainsUppercase;
+    } else if (!AuthValidatorRegex.passwordHasLowercaseCharacter(value)) {
+      return AppStrings.passwordContainsLowercase;
+    } else if (!AuthValidatorRegex.passwordHasNumber(value)) {
+      return AppStrings.passwordContainsDigit;
+    } else if (!AuthValidatorRegex.passwordHasSpecialCharacter(value)) {
+      return AppStrings.passwordContainsSpecial;
+    }
+    return null;
+  }
+
+  static String? validateEmailField(BuildContext context, {String? value}) {
+    if (value == null || value.isEmpty) {
+      return AppStrings.emailRequired;
+    } else if (!AuthValidatorRegex.isEmailValid(value)) {
+      return AppStrings.emailInvalid;
+    }
+    return null;
+  }
+}
