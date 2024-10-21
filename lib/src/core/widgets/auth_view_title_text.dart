d8d0f6cdb85faa1b940f79f94e8fb2865020180b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-21 15:09:00 +0300

    Built SlideAnimatedCareyIcon, AuthViewTitleText widgets & Updated LoginMethodsView

---
+++ lib/src/core/widgets/auth_view_title_text.dart
@@ -0,0 +1,19 @@
+import 'package:carey/src/core/themes/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class AuthViewTitleText extends StatelessWidget {
+  const AuthViewTitleText({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: AppTextStyles.poppinsFont45Medium,
+    );
+  }
+}
--- lib/src/features/login/presentation/views/login_methods_view.dart
+++ lib/src/features/login/presentation/views/login_methods_view.dart
@@ -1,16 +1,14 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:carey/src/core/utils/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:carey/src/core/themes/app_text_styles.dart';
-import 'package:carey/src/core/utils/app_assets.dart';
+import 'package:carey/src/core/utils/app_constants.dart';
 import 'package:carey/src/core/utils/app_strings.dart';
 import 'package:carey/src/core/widgets/auth_custom_divider_with_text.dart';
 import 'package:carey/src/core/widgets/auth_switcher.dart';
-import 'package:carey/src/core/widgets/my_sized_box.dart';
+import 'package:carey/src/core/widgets/auth_view_title_text.dart';
 import 'package:carey/src/core/widgets/primary_button.dart';
-import 'package:carey/src/core/widgets/slide_animated_widget.dart';
+import 'package:carey/src/core/widgets/slide_animated_carey_icon.dart';
 import 'package:carey/src/features/login/presentation/widgets/social_login_buttons.dart';
 
 @RoutePage()
@@ -26,17 +24,8 @@ class LoginMethodsView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              MySizedBox.height40,
-              SlideAnimatedWidget(
-                curve: Curves.fastLinearToSlowEaseIn,
-                duration: const Duration(seconds: 3),
-                begin: const Offset(-1, 0),
-                child: Image.asset(Assets.careyIcon),
-              ),
-              Text(
-                AppStrings.letsSignYouIn,
-                style: AppTextStyles.poppinsFont45Medium,
-              ),
+              const SlideAnimatedCareyIcon(),
+              const AuthViewTitleText(title: AppStrings.letsSignYouIn),
               Container(
                 margin: EdgeInsetsDirectional.only(top: 16.h, bottom: 48.h),
                 child: const SocialLoginButtons(),
