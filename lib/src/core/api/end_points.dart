a2f39300c66bc484eeb2bd71cb944c4fc1a506d9
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-21 17:31:32 +0300

    Built LoginResponse model & Added EndPoints

---
+++ lib/src/core/api/end_points.dart
@@ -0,0 +1,7 @@
+class EndPoints {
+  EndPoints._();
+
+  static const String baseUrl = 'https://carey-production.up.railway.app/';
+  static const String auth = '${baseUrl}auth/';
+  static const String login = '${auth}login';
+}
