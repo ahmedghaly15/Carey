0061519d3be2dfc5319f80ee78f9b399585655e8
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-22 13:47:16 +0300

    Finished Register UI & Updated EmailTextFormField widget

---
+++ lib/src/features/auth/presentation/views/register_view.dart
@@ -0,0 +1,37 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:carey/src/core/widgets/arrow_back_icon_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:carey/src/core/di/dependency_injection.dart';
+import 'package:carey/src/features/auth/presentation/cubits/auth_form_attributes/form_attributes_cubit.dart';
+import 'package:carey/src/features/auth/presentation/widgets/register/register_view_body.dart';
+
+@RoutePage()
+class RegisterView extends StatelessWidget implements AutoRouteWrapper {
+  const RegisterView({super.key, this.isPushedFromLogin = true});
+
+  final bool isPushedFromLogin;
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return BlocProvider<FormAttributesCubit>(
+      create: (_) => getIt.get<FormAttributesCubit>(),
+      child: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: context.router.canPop()
+          ? AppBar(
+              leading: const ArrowBackIconButton(),
+            )
+          : null,
+      body: SafeArea(
+        child: RegisterViewBody(isPushedFromLogin: isPushedFromLogin),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/auth/presentation/widgets/register/register_view_body.dart
@@ -0,0 +1,61 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:carey/src/core/router/app_router.dart';
+import 'package:carey/src/core/utils/app_assets.dart';
+import 'package:carey/src/core/utils/app_constants.dart';
+import 'package:carey/src/core/utils/app_strings.dart';
+import 'package:carey/src/core/widgets/my_sized_box.dart';
+import 'package:carey/src/core/widgets/primary_button.dart';
+import 'package:carey/src/features/auth/presentation/widgets/auth_custom_divider_with_text.dart';
+import 'package:carey/src/features/auth/presentation/widgets/auth_form.dart';
+import 'package:carey/src/features/auth/presentation/widgets/auth_switcher.dart';
+import 'package:carey/src/features/auth/presentation/widgets/auth_view_title_text.dart';
+import 'package:carey/src/features/auth/presentation/widgets/remember_me_bloc_builder.dart';
+import 'package:carey/src/features/auth/presentation/widgets/social_login_icon_buttons.dart';
+
+class RegisterViewBody extends StatelessWidget {
+  const RegisterViewBody({super.key, this.isPushedFromLogin = true});
+
+  final bool isPushedFromLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: AppConstants.screenHorizontalPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(Assets.careyIcon),
+            const AuthViewTitleText(title: AppStrings.createYourAccount),
+            MySizedBox.height40,
+            const AuthForm(),
+            const RememberMeBlocBuilder(),
+            PrimaryButton(
+              onPressed: () {},
+              text: AppStrings.signUp,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 19.h),
+              child: const AuthCustomDividerWithText(
+                text: '${AppStrings.or} ${AppStrings.continueWith}',
+              ),
+            ),
+            const SocialLoginIconButtons(),
+            AuthSwitcher(
+              buttonText: AppStrings.signIn,
+              question: AppStrings.alreadyHaveAnAccount,
+              onPressed: () {
+                isPushedFromLogin
+                    ? context.maybePop()
+                    : context.pushRoute(LoginRoute(isPushedFromRegister: true));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
