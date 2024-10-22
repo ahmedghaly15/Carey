c31dc72561dd1f38713cd836e432b8da3f2456c6
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-22 23:46:31 +0300

    Added checkIfCurrentUserAccountSet logic

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:carey/src/carey_app.dart';
 import 'package:carey/src/core/di/dependency_injection.dart';
 import 'package:carey/src/core/helpers/bloc_observer.dart';
+import 'package:carey/src/core/utils/functions/check_if_current_user_account_set.dart';
 import 'package:carey/src/core/utils/functions/check_if_user_is_logged_in.dart';
 
 void main() async {
@@ -11,5 +12,6 @@ void main() async {
   await setupDI();
   Bloc.observer = MyBlocObserver();
   await checkIfUserIsLoggedIn();
+  await checkIfCurrentUserAccountSet();
   runApp(const CareyApp());
 }
