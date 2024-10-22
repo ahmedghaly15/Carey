737dd59d1a14ffb76eb9d12302c29dc2c8e5fa56
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-22 15:11:39 +0300

    Finished Register UI logic

---
+++ lib/src/features/auth/presentation/cubits/register/register_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:carey/src/features/auth/data/models/auth_via_password_request.dart';
+import 'package:carey/src/features/auth/data/repositories/register_repo.dart';
+import 'package:carey/src/features/auth/presentation/cubits/register/register_state.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  final RegisterRepo _registerRepo;
+
+  RegisterCubit(this._registerRepo) : super(const RegisterState.initial());
+
+  final CancelToken _cancelToken = CancelToken();
+
+  void register(AuthViaPasswordRequest params) async {
+    emit(const RegisterState.registerLoading());
+    final result = await _registerRepo.register(params, _cancelToken);
+    result.when(
+      success: (authEntity) => emit(RegisterState.registerSuccess(authEntity)),
+      failure: (failure) => emit(RegisterState.registerError(failure.error[0])),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _cancelToken.cancel();
+    return super.close();
+  }
+}
