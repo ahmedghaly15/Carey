f8d30aec72400b3b0046d508ab272ccfea841060
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-28 11:20:24 +0300

    Finished PinCodeVerification UI logic

---
+++ lib/src/features/auth/presentation/cubits/pin_code_verification/pin_code_verification_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'pin_code_verification_state.freezed.dart';
+
+@freezed
+class PinCodeVerificationState<T> with _$PinCodeVerificationState<T> {
+  const factory PinCodeVerificationState.initial() = _Initial;
+  const factory PinCodeVerificationState.verifyLoading() = VerifyLoading;
+  const factory PinCodeVerificationState.verifySuccess() = VerifySuccess;
+  const factory PinCodeVerificationState.verifyError(String error) =
+      VerifyError<T>;
+}
