03426f5372f364c4d166f307c44e4dfba0379a7b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-04 21:07:48 +0200

    Built Init version of HomeView & Updated AppRouter, appRoutes

---
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Wow!, It\'s Home',
+          style: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+}
