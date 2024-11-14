d3ced6d46e622c95c51bbe301864bb920011689d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-14 11:10:09 +0200

    Built TopDealsBrandsListView widget & Updated HomeBody widget

--- lib/src/features/home/presentation/widgets/home_body.dart
+++ lib/src/features/home/presentation/widgets/home_body.dart
@@ -8,6 +8,7 @@ import 'package:carey/src/features/home/presentation/widgets/home_custom_search_
 import 'package:carey/src/features/home/presentation/widgets/home_sliver_app_bar.dart';
 import 'package:carey/src/features/home/presentation/widgets/special_offers_page_view.dart';
 import 'package:carey/src/features/home/presentation/widgets/text_and_see_all.dart';
+import 'package:carey/src/features/home/presentation/widgets/top_deals_brands_list_view.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
@@ -15,43 +16,58 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppConstants.screenHorizontalPaddingVal.w,
-        ),
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverPadding(
-              padding: EdgeInsetsDirectional.symmetric(vertical: 24.h),
-              sliver: const HomeSliverAppBar(),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverPadding(
+            padding: EdgeInsetsDirectional.symmetric(
+              vertical: 24.h,
+              horizontal: AppConstants.screenHorizontalPaddingVal.w,
             ),
-            const SliverToBoxAdapter(
+            sliver: const HomeSliverAppBar(),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppConstants.screenHorizontalPaddingVal.w,
+            ),
+            sliver: const SliverToBoxAdapter(
               child: HomeCustomSearchField(),
             ),
-            SliverPadding(
-              padding: EdgeInsetsDirectional.symmetric(vertical: 16.h),
-              sliver: SliverToBoxAdapter(
-                child: TextAndSeeAll(
-                  text: AppStrings.specialOffers,
-                  seeAllOnPressed: () {},
-                ),
-              ),
+          ),
+          SliverToBoxAdapter(
+            child: TextAndSeeAll(
+              text: AppStrings.specialOffers,
+              seeAllOnPressed: () {},
             ),
-            const SliverToBoxAdapter(
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppConstants.screenHorizontalPaddingVal.w,
+            ),
+            sliver: const SliverToBoxAdapter(
               child: SpecialOffersPageView(),
             ),
-            SliverPadding(
-              padding: EdgeInsetsDirectional.only(top: 16.h),
-              sliver: const CarBrandsSliverGrid(),
+          ),
+          SliverPadding(
+            padding: EdgeInsetsDirectional.only(
+              top: 16.h,
+              start: AppConstants.screenHorizontalPaddingVal.w,
+              end: AppConstants.screenHorizontalPaddingVal.w,
             ),
-            SliverToBoxAdapter(
-              child: TextAndSeeAll(
-                text: AppStrings.topDeals,
-                seeAllOnPressed: () {},
-              ),
+            sliver: const CarBrandsSliverGrid(),
+          ),
+          SliverToBoxAdapter(
+            child: TextAndSeeAll(
+              text: AppStrings.topDeals,
+              seeAllOnPressed: () {},
             ),
-          ],
-        ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 34.h,
+              child: const TopDealsBrandsListView(),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/src/features/home/presentation/widgets/top_deals_brands_list_view.dart
@@ -0,0 +1,45 @@
+import 'package:carey/src/core/themes/app_text_styles.dart';
+import 'package:carey/src/core/utils/app_constants.dart';
+import 'package:carey/src/core/widgets/my_sized_box.dart';
+import 'package:carey/src/core/widgets/primary_button.dart';
+import 'package:carey/src/features/home/presentation/cubit/home_cubit.dart';
+import 'package:carey/src/features/home/presentation/cubit/home_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TopDealsBrandsListView extends StatelessWidget {
+  const TopDealsBrandsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: EdgeInsetsDirectional.only(
+        start: AppConstants.screenHorizontalPaddingVal.w,
+        end: AppConstants.screenHorizontalPaddingVal.w,
+      ),
+      scrollDirection: Axis.horizontal,
+      itemCount: 12,
+      itemBuilder: (_, index) => BlocSelector<HomeCubit, HomeState, int>(
+        selector: (state) => state.currentSelectedTopDealBrand,
+        builder: (context, currentSelectedTopDealBrand) {
+          final bool isSelected = currentSelectedTopDealBrand == index;
+          return PrimaryButton(
+            padding: EdgeInsets.symmetric(
+              vertical: 6.h,
+              horizontal: 9.w,
+            ),
+            isOutlined: isSelected ? false : true,
+            textStyle: AppTextStyles.font16Bold.copyWith(
+              color: isSelected ? Colors.white : Colors.black,
+            ),
+            onPressed: () =>
+                context.read<HomeCubit>().updateSelectedTopDealBrand(index),
+            text: 'Mercedes',
+          );
+        },
+      ),
+      separatorBuilder: (_, __) => MySizedBox.width9,
+    );
+  }
+}
