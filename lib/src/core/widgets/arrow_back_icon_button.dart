0dab71ff656ad1ced66c93010781eb567cdd765c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-16 13:57:17 +0200

    Updated PrimaryButton & Changed FormAttributes to AuthForm

--- lib/src/core/widgets/arrow_back_icon_button.dart
+++ lib/src/core/widgets/arrow_back_icon_button.dart
@@ -1,7 +1,8 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:carey/src/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:carey/src/core/utils/app_assets.dart';
 
 class ArrowBackIconButton extends StatelessWidget {
   const ArrowBackIconButton({super.key});
