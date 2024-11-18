eb9a0b0707dcf24693c0280e1c5d02fce78cefb2
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-18 03:57:45 +0200

    Updated updateSelectedOffer UI logic

--- lib/src/features/make_offer/presentation/cubit/make_offer_cubit.dart
+++ lib/src/features/make_offer/presentation/cubit/make_offer_cubit.dart
@@ -4,25 +4,44 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class MakeOfferCubit extends Cubit<MakeOfferState> {
   MakeOfferCubit() : super(MakeOfferState.initial());
 
-  void _updateSelectedOffer(int offer) {
-    if (offer != state.selectedOffer) {
-      emit(state.copyWith(
-        status: MakeOfferStateStatus.updateSelectedOffer,
-        selectedOffer: offer,
-      ));
-    }
+  void _updateSelectedOfferAmount(int offer) {
+    emit(state.copyWith(
+      status: MakeOfferStateStatus.updateSelectedOffer,
+      selectedOffer: offer,
+    ));
   }
 
-  void updateSelectedOfferIndex({
+  void updateSelectedOffer({
     required int index,
     required int offer,
   }) {
     if (state.selectedOfferIndex != index) {
-      emit(state.copyWith(
-        status: MakeOfferStateStatus.updateSelectedOfferIndex,
-        selectedOfferIndex: index,
-      ));
-      _updateSelectedOffer(offer);
+      _updatedSelectedOfferIndex(index);
+      _updateSelectedOfferAmount(offer);
+    } else {
+      _unSelectTheOffer();
+      _makeSelectedOfferZero();
     }
+  }
+
+  void _updatedSelectedOfferIndex(int index) {
+    emit(state.copyWith(
+      status: MakeOfferStateStatus.updateSelectedOfferIndex,
+      selectedOfferIndex: index,
+    ));
+  }
+
+  void _makeSelectedOfferZero() {
+    emit(state.copyWith(
+      status: MakeOfferStateStatus.updateSelectedOffer,
+      selectedOffer: 0,
+    ));
+  }
+
+  void _unSelectTheOffer() {
+    emit(state.copyWith(
+      status: MakeOfferStateStatus.updateSelectedOfferIndex,
+      selectedOfferIndex: -1,
+    ));
   }
 }
