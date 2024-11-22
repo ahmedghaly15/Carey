79d70d79d35779a7388b32d5b41f378227933a8a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-23 00:25:21 +0200

    Finished FetchHome business logic

---
+++ lib/src/features/home/data/api/home_api_service.dart
@@ -0,0 +1,14 @@
+import 'package:carey/src/core/api/end_points.dart';
+import 'package:carey/src/features/home/data/models/fetch_home_response.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'home_api_service.g.dart';
+
+@RestApi(baseUrl: EndPoints.baseUrl)
+abstract class HomeApiService {
+  factory HomeApiService(Dio dio, {String baseUrl}) = _HomeApiService;
+
+  @GET(EndPoints.fetchHome)
+  Future<FetchHomeResponse> fetchHome();
+}
---
+++ lib/src/features/home/data/repositories/home_repo.dart
@@ -0,0 +1,17 @@
+import 'package:carey/src/core/api/api_result.dart';
+import 'package:carey/src/core/utils/functions/execute_and_handle_errors.dart';
+import 'package:carey/src/features/home/data/api/home_api_service.dart';
+import 'package:carey/src/features/home/data/models/fetch_home_response.dart';
+
+class HomeRepo {
+  final HomeApiService _homeApiService;
+
+  HomeRepo(this._homeApiService);
+
+  Future<ApiResult<HomeResponseData>> fetchHome() {
+    return executeAndHandleErrors<HomeResponseData>(() async {
+      final homeResponse = await _homeApiService.fetchHome();
+      return homeResponse.data;
+    });
+  }
+}
