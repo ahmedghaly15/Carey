b446cc2b890f84560109844cd6fef93fcf63fde5
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-26 15:29:07 +0200

    Updated AuthLocalDatasource to UserLocalDatasource

--- lib/src/core/datasources/user_local_data_source.dart
+++ lib/src/core/datasources/user_local_data_source.dart
@@ -3,11 +3,19 @@ import 'dart:convert';
 import 'package:carey/src/core/api/dio_factory.dart';
 import 'package:carey/src/core/helpers/cache_keys.dart';
 import 'package:carey/src/core/helpers/secure_storage_helper.dart';
+import 'package:carey/src/core/models/carey_user.dart';
 import 'package:carey/src/core/utils/app_constants.dart';
 import 'package:carey/src/features/auth/domain/entities/auth_response_entity.dart';
+import 'package:flutter/material.dart';
 
-class AuthLocalDataSource {
-  AuthLocalDataSource._();
+class UserLocalDatasource {
+  UserLocalDatasource._();
+
+  static Future<void> updateAndSecureCurrentUser(CareyUser user) async {
+    currentUserData = currentUserData!.copyWith(user: user);
+    debugPrint('CURRENT USER : ${currentUserData!.toJson()}');
+    await secureUserData(currentUserData!);
+  }
 
   static Future<void> secureUserData(AuthResponseEntity userData) async {
     await SecureStorageHelper.setSecuredString(
