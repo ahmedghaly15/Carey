39045342f5064db37402d92a8563b3c811582f95
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-26 15:38:44 +0200

    Updated FillProfileContinueBlocListener widget  - Updated _listenWhen() & _listener() methods

--- lib/src/features/auth/presentation/widgets/account_setup/fill_profile_continue_bloc_listener.dart
+++ lib/src/features/auth/presentation/widgets/account_setup/fill_profile_continue_bloc_listener.dart
@@ -1,14 +1,15 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:carey/src/core/router/app_router.dart';
-import 'package:carey/src/features/auth/data/models/update_profile_params.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'package:carey/src/core/datasources/user_local_data_source.dart';
 import 'package:carey/src/core/helpers/extensions.dart';
+import 'package:carey/src/core/router/app_router.dart';
 import 'package:carey/src/core/utils/app_constants.dart';
 import 'package:carey/src/core/utils/app_strings.dart';
 import 'package:carey/src/core/widgets/primary_button.dart';
+import 'package:carey/src/features/auth/data/models/update_profile_params.dart';
 import 'package:carey/src/features/auth/presentation/cubits/account_setup/account_setup_cubit.dart';
 import 'package:carey/src/features/auth/presentation/cubits/account_setup/account_setup_state.dart';
 
@@ -35,31 +36,20 @@ class FillProfileContinueBlocListener extends StatelessWidget {
   Future<void> _listener(AccountSetupState state, BuildContext context) async {
     switch (state.status) {
       case AccountSetupStateStatus.updateProfileLoading:
-        _handleUpdateProfileLoading(context);
+        context.unfocusKeyboard();
+        context.showLoadingDialog();
         break;
       case AccountSetupStateStatus.updateProfileSuccess:
         await _handleUpdateProfileSuccess(state, context);
         break;
       case AccountSetupStateStatus.updateProfileError:
-        _handleUpdateProfileError(context, state);
-        break;
       case AccountSetupStateStatus.updateProfileImgError:
-        _handleUpdateProfileError(context, state);
+        context.popTop();
+        context.showErrorDialog(state.error!);
         break;
       default:
         break;
     }
-  }
-
-  void _handleUpdateProfileError(
-      BuildContext context, AccountSetupState state) {
-    context.popTop();
-    context.showErrorDialog(state.error!);
-  }
-
-  void _handleUpdateProfileLoading(BuildContext context) {
-    context.unfocusKeyboard();
-    context.showLoadingDialog();
   }
 
   Future<void> _handleUpdateProfileSuccess(
@@ -70,16 +60,18 @@ class FillProfileContinueBlocListener extends StatelessWidget {
     if (state.pickedProfileImg != null) {
       await accountSetupCubit.updateProfileImg();
     }
+    await UserLocalDatasource.updateAndSecureCurrentUser(state.careyUser!);
     context.popTop();
-    final updateProfileParams = UpdateProfileParams(
-      fullName: accountSetupCubit.fullNameController.text,
-      nickName: accountSetupCubit.nickNameController.text,
-      gender: accountSetupCubit.genderController.text,
-      address: accountSetupCubit.addressController.text,
-      phone: accountSetupCubit.phoneNumber,
-    );
     context.pushRoute(
-      SetFingerprintRoute(updateProfileParams: updateProfileParams),
+      SetFingerprintRoute(
+        updateProfileParams: UpdateProfileParams(
+          fullName: accountSetupCubit.fullNameController.text,
+          nickName: accountSetupCubit.nickNameController.text,
+          gender: accountSetupCubit.genderController.text,
+          address: accountSetupCubit.addressController.text,
+          phone: accountSetupCubit.phoneNumber,
+        ),
+      ),
     );
   }
 
