c5bab49a352af850a8ea786d6ec83f2c34913aed
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-03-10 17:24:38 +0200

    Updated Checkout UI logic  - Handled updating shipping method

--- lib/src/features/checkout/presentation/cubit/checkout_cubit.dart
+++ lib/src/features/checkout/presentation/cubit/checkout_cubit.dart
@@ -1,16 +1,26 @@
+import 'package:carey/src/features/checkout/data/models/shipping_method.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:carey/src/features/checkout/data/models/shipping_item.dart';
+import 'package:carey/src/features/checkout/data/models/shipping_address.dart';
 import 'package:carey/src/features/checkout/presentation/cubit/checkout_state.dart';
 
 class CheckoutCubit extends Cubit<CheckoutState> {
   CheckoutCubit() : super(CheckoutState.initial());
 
-  void updateSelectedShippingAddress(ShippingItem shippingItem) {
-    if (state.selectedShippingAddress != shippingItem) {
+  void updateShippingAddress(ShippingAddress shippingAddress) {
+    if (state.selectedShippingAddress != shippingAddress) {
       emit(state.copyWith(
-        status: CheckoutStateStatus.updateSelectedShippingAddress,
-        selectedShippingAddress: shippingItem,
+        status: CheckoutStateStatus.updateShippingAddress,
+        selectedShippingAddress: shippingAddress,
+      ));
+    }
+  }
+
+  void updateShippingMethod(ShippingMethod shippingMethod) {
+    if (state.selectedShippingMethod != shippingMethod) {
+      emit(state.copyWith(
+        status: CheckoutStateStatus.updateShippingMethod,
+        selectedShippingMethod: shippingMethod,
       ));
     }
   }
